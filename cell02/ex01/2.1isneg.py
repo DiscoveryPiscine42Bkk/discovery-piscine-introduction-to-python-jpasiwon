c2c2b89583e0e0fd3771c13fa2dc8fdec9c5efd9
jpasiwon <jiraphon.pasiwong@gmail.com>
2025-06-17 03:37:05 +0000

    2.1isneg.py

---
+++ cell02/ex01/2.1isneg.py
@@ -0,0 +1,8 @@
+number = float(input(" Please Enter Number : "))
+
+if number < 0:
+    print("This Number is negative. ")
+elif number > 0:
+    print("This Number is Positive. ")
+else :
+    print("This Number is both positive and negative.")
